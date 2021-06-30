0e470bfd4818d31d54a12deecf592f2c30113cbd
markopolo139 <marekseget@onet.pl>
2021-06-30 14:57:26 +0200

    feat: create basic structure for project

---
+++ src/main/java/com/FireEmbelm/FireEmblem/FireEmblemApplication.java
@@ -0,0 +1,13 @@
+package com.FireEmbelm.FireEmblem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FireEmblemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FireEmblemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/FireEmbelm/FireEmblem/FireEmblemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.FireEmbelm.FireEmblem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FireEmblemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
