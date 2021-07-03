fdd4d3978ffc16f600aa8de16c15ac22d6e68f0c
markopolo139 <marekseget@onet.pl>
2021-07-03 16:26:45 +0200

    feat(utils): create method to create base stats

--- src/main/java/com/FireEmbelm/FireEmblem/business/utils/Utils.java
+++ src/main/java/com/FireEmbelm/FireEmblem/business/utils/Utils.java
@@ -1,6 +1,7 @@
 package com.FireEmbelm.FireEmblem.business.utils;
 
 import com.FireEmbelm.FireEmblem.business.exceptions.InvalidRankException;
+import com.FireEmbelm.FireEmblem.business.value.Stats;
 
 public class Utils {
 
@@ -21,5 +22,26 @@ public class Utils {
             default:
                 throw new InvalidRankException();
         }
+    }
+
+    public static Stats[] createStats(
+            int hpValue, int hpChances,
+            int strValue, int strChances,
+            int magValue, int magChances,
+            int skillValue, int skillChances,
+            int spdValue, int spdChances,
+            int luckValue, int luckChances,
+            int defValue, int defChances,
+            int resValue, int resChances){
+        return new Stats[] {
+                Stats.HEALTH.setValueAndChances(hpValue, hpChances),
+                Stats.STRENGTH.setValueAndChances(strValue, strChances),
+                Stats.MAGICK.setValueAndChances(magValue, magChances),
+                Stats.SKILL.setValueAndChances(skillValue, skillChances),
+                Stats.SPEED.setValueAndChances(spdValue, spdChances),
+                Stats.LUCK.setValueAndChances(luckValue, luckChances),
+                Stats.DEFENSE.setValueAndChances(defValue, defChances),
+                Stats.RESISTANCE.setValueAndChances(resValue, resChances)
+        };
     }
 }
