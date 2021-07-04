f916676dd622c7db74998c40906de80209e8d735
markopolo139 <marekseget@onet.pl>
2021-07-04 18:32:56 +0200

    feat(config): add field generator to config

--- src/main/java/com/FireEmbelm/FireEmblem/app/configuration/BusinessConfiguration.java
+++ src/main/java/com/FireEmbelm/FireEmblem/app/configuration/BusinessConfiguration.java
@@ -1,6 +1,7 @@
 package com.FireEmbelm.FireEmblem.app.configuration;
 
 import com.FireEmbelm.FireEmblem.business.service.ShopService;
+import com.FireEmbelm.FireEmblem.business.service.generators.FieldGenerator;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Scope;
@@ -13,5 +14,11 @@ public class BusinessConfiguration {
     @Scope("Singleton")
     public ShopService getShopService() {
         return new ShopService();
+    }
+
+    @Bean
+    @Scope("Singleton")
+    public FieldGenerator getFieldGenerator() {
+        return new FieldGenerator();
     }
 }
