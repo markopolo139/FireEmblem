de4c2e5f952a78b76803881fbc3cc6760af12eb0
markopolo139 <marekseget@onet.pl>
2021-07-04 15:59:07 +0200

    feat(config): create configuration for services

---
+++ src/main/java/com/FireEmbelm/FireEmblem/app/configuration/BusinessConfiguration.java
@@ -0,0 +1,17 @@
+package com.FireEmbelm.FireEmblem.app.configuration;
+
+import com.FireEmbelm.FireEmblem.business.service.ShopService;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Scope;
+
+//TODO : to create shopService use method to generate random items from interactor, and getting all items from base
+@Configuration
+public class BusinessConfiguration {
+
+    @Bean
+    @Scope("Singleton")
+    public ShopService getShopService() {
+        return new ShopService();
+    }
+}
