a9b4cb5935a4812e4b33ea6dbc2f60df3d9d0094
markopolo139 <marekseget@onet.pl>
2021-07-07 15:23:17 +0200

    feat(seals): create basics for promoting

--- src/main/java/com/FireEmbelm/FireEmblem/business/value/equipment/Seals.java
+++ src/main/java/com/FireEmbelm/FireEmblem/business/value/equipment/Seals.java
@@ -1,21 +1,35 @@
 package com.FireEmbelm.FireEmblem.business.value.equipment;
 
+import com.FireEmbelm.FireEmblem.business.entitie.Character;
 import com.FireEmbelm.FireEmblem.business.value.categories.ConsumableItemCategory;
 import com.FireEmbelm.FireEmblem.business.value.categories.ItemCategory;
 
+//TODO : create promoting
 public enum Seals implements Equipment {
     MASTER_SEAL(
             "Master Seal", 2500,
             "Master Seal - enable to promote to higher class from lv 15"
-    ),
+    ) {
+        @Override
+        public void promoteCharacter(Character character) {
+
+        }
+    },
     HEART_SEAL(
             "Heart Seal", 2000,
             "Heart Seal - enable to change to another class, chosen randomly"
-            );
+    ){
+        @Override
+        public void promoteCharacter(Character character) {
+
+        }
+    };
 
     private final String mItemName;
     private final int mWorth;
     private final String mDescription;
+
+    public abstract void promoteCharacter(Character character);
 
     Seals(String itemName, int worth, String description) {
         mItemName = itemName;
