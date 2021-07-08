cd84f5490434d40af982dd292085e098ad0858f9
markopolo139 <marekseget@onet.pl>
2021-07-08 09:31:06 +0200

    feat(character class): add promoted class

--- src/main/java/com/FireEmbelm/FireEmblem/business/entitie/CharacterClass.java
+++ src/main/java/com/FireEmbelm/FireEmblem/business/entitie/CharacterClass.java
@@ -12,8 +12,24 @@ import java.util.Collection;
 import java.util.Collections;
 import java.util.HashMap;
 
-//TODO : add promoted classes
 public enum CharacterClass  {
+    GREAT_LORD(
+            "Great Lord",
+            6,
+            Arrays.asList(WeaponCategory.SWORD, WeaponCategory.LANCE),
+            Utils.createStats(
+                    23,40,
+                    10,20,
+                    0,0,
+                    7,20,
+                    9,20,
+                    0,0,
+                    10,10,
+                    3,5
+            ),
+            true,
+            Collections.emptyList()
+    ),
     LORD(
             "Lord",
             5,
@@ -28,7 +44,25 @@ public enum CharacterClass  {
                     7,10,
                     0,5
             ),
-            false
+            false,
+            Collections.singletonList(GREAT_LORD)
+    ),
+    PALADIN(
+            "Paladin",
+            8,
+            Arrays.asList(WeaponCategory.SWORD, WeaponCategory.LANCE),
+            Utils.createStats(
+                    25,45,
+                    9,20,
+                    1,0,
+                    7,20,
+                    8,20,
+                    0,0,
+                    10,10,
+                    6,10
+            ),
+            true,
+            Collections.emptyList()
     ),
     CAVALRY(
             "Cavalry",
@@ -44,7 +78,25 @@ public enum CharacterClass  {
                     7,10,
                     0,5
             ),
-            false
+            false,
+            Collections.singletonList(PALADIN)
+    ),
+    GENERAL(
+            "General",
+            5,
+            Arrays.asList(WeaponCategory.AXE, WeaponCategory.LANCE),
+            Utils.createStats(
+                    28,50,
+                    12,25,
+                    0,0,
+                    7,15,
+                    4,10,
+                    0,0,
+                    15,15,
+                    3,10
+            ),
+            true,
+            Collections.emptyList()
     ),
     KNIGHT(
             "Knight",
@@ -60,7 +112,25 @@ public enum CharacterClass  {
                     11,15,
                     0,5
             ),
-            false
+            false,
+            Collections.singletonList(GENERAL)
+    ),
+    SWORDMASTER(
+            "Swordmaster",
+            6,
+            Collections.singletonList(WeaponCategory.SWORD),
+            Utils.createStats(
+                    20,40,
+                    7,20,
+                    2,0,
+                    11,25,
+                    13,25,
+                    0,0,
+                    6,5,
+                    4,10
+            ),
+            true,
+            Collections.emptyList()
     ),
     MYRMIDON(
             "Myrmidon",
@@ -76,7 +146,25 @@ public enum CharacterClass  {
                     4,5,
                     1,5
             ),
-            false
+            false,
+            Collections.singletonList(SWORDMASTER)
+    ),
+    WARRIOR(
+            "Warrior",
+            6,
+            Collections.singletonList(WeaponCategory.AXE),
+            Utils.createStats(
+                    28,45,
+                    12,25,
+                    0,0,
+                    8,20,
+                    7,15,
+                    0,0,
+                    7,10,
+                    3,5
+            ),
+            true,
+            Collections.emptyList()
     ),
     FIGHTER(
             "Fighter",
@@ -92,7 +180,25 @@ public enum CharacterClass  {
                     4,10,
                     0,5
             ),
-            false
+            false,
+            Collections.singletonList(WARRIOR)
+    ),
+    HERO(
+            "Hero",
+            6,
+            Arrays.asList(WeaponCategory.SWORD, WeaponCategory.AXE),
+            Utils.createStats(
+                    22,45,
+                    8,20,
+                    1,0,
+                    11,25,
+                    10,20,
+                    0,0,
+                    8,10,
+                    3,5
+            ),
+            true,
+            Collections.emptyList()
     ),
     MERCENARY(
             "Mercenary",
@@ -108,7 +214,25 @@ public enum CharacterClass  {
                     5,10,
                     0,5
             ),
-            false
+            false,
+            Collections.singletonList(HERO)
+    ),
+    SNIPER(
+            "Sniper",
+            6,
+            Collections.singletonList(WeaponCategory.BOW),
+            Utils.createStats(
+                    20,45,
+                    7,15,
+                    1,0,
+                    12,30,
+                    9,15,
+                    0,0,
+                    10,15,
+                    3,5
+            ),
+            true,
+            Collections.emptyList()
     ),
     ARCHER(
             "Archer",
@@ -124,7 +248,25 @@ public enum CharacterClass  {
                     5,10,
                     0,5
             ),
-            false
+            false,
+            Collections.singletonList(SNIPER)
+    ),
+    SAGE(
+            "Sage",
+            6,
+            Collections.singletonList(WeaponCategory.TOME),
+            Utils.createStats(
+                    20,35,
+                    1,0,
+                    7,20,
+                    5,20,
+                    7,20,
+                    0,0,
+                    4,5,
+                    5,10
+            ),
+            true,
+            Collections.emptyList()
     ),
     MAGE(
             "Mage",
@@ -140,12 +282,30 @@ public enum CharacterClass  {
                     2,5,
                     3,10
             ),
-            false
+            false,
+            Collections.singletonList(SAGE)
+    ),
+    WAR_MONK(
+            "War monk",
+            6,
+            Arrays.asList(WeaponCategory.AXE, WeaponCategory.STAFF),
+            Utils.createStats(
+                    24,45,
+                    5,15,
+                    5,15,
+                    4,10,
+                    6,15,
+                    0,0,
+                    6,10,
+                    6,10
+            ),
+            true,
+            Collections.emptyList()
     ),
     PRIEST(
             "Priest",
             5,
-            Collections.singletonList(ConsumableItemCategory.STAFF),
+            Collections.singletonList(WeaponCategory.STAFF),
             Utils.createStats(
                     16,35,
                     0,5,
@@ -156,14 +316,17 @@ public enum CharacterClass  {
                     1,5,
                     6,15
             ),
-            false
-    );
+            false,
+            Collections.singletonList(WAR_MONK)
+    )
+    ;
 
     private final String mClassName;
     private final int mMovement;
     private final Collection<ItemCategory> mAllowedWeapons;
     private final HashMap<StatsType, Stats> mBonusStats;
     private final boolean mPromotedClass;
+    private final Collection<CharacterClass> mPromoteToClasses;
 
     public String getClassName() {
         return mClassName;
@@ -185,14 +348,19 @@ public enum CharacterClass  {
         return mPromotedClass;
     }
 
+    public Collection<CharacterClass> getPromoteToClasses() {
+        return mPromoteToClasses;
+    }
+
     CharacterClass(
             String className, int movement, Collection<ItemCategory> allowedWeapons,
-            HashMap<StatsType, Stats> bonusStats, boolean promotedClass
+            HashMap<StatsType, Stats> bonusStats, boolean promotedClass, Collection<CharacterClass> promoteToClasses
     ) {
         mClassName = className;
         mMovement = movement;
         mAllowedWeapons = allowedWeapons;
         mBonusStats = bonusStats;
         mPromotedClass = promotedClass;
+        mPromoteToClasses = promoteToClasses;
     }
 }
