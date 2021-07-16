e1f6121907eae8c46467eae8c635bc7114fd1a44
markopolo139 <marekseget@onet.pl>
2021-07-16 12:39:32 +0200

    feat(entity): create base character entity

---
+++ src/main/java/com/FireEmbelm/FireEmblem/app/data/entities/BaseCharacterEntity.java
@@ -0,0 +1,80 @@
+package com.FireEmbelm.FireEmblem.app.data.entities;
+
+import com.FireEmbelm.FireEmblem.app.data.entities.embeddable.EquipmentEmbeddable;
+import com.FireEmbelm.FireEmblem.app.data.entities.embeddable.StatEmbeddable;
+import com.FireEmbelm.FireEmblem.business.entitie.CharacterClass;
+import com.FireEmbelm.FireEmblem.business.value.character.related.CharacterState;
+import org.hibernate.annotations.NaturalId;
+import org.springframework.lang.Nullable;
+
+import javax.persistence.*;
+import java.util.List;
+import java.util.Objects;
+
+@Entity
+@Table(name = "base_characters ")
+public class BaseCharacterEntity {
+
+    @Id
+    @GeneratedValue
+    public Long characterId;
+
+    @NaturalId
+    public String name;
+
+    public int level;
+
+    public int exp;
+
+    public int remainingHealth;
+
+    @Nullable
+    public Integer currentEquipedItemId;
+
+    @Enumerated(EnumType.STRING)
+    public CharacterClass characterClass;
+
+    @Enumerated(EnumType.STRING)
+    public CharacterState characterState;
+
+    public boolean moved;
+
+    @ElementCollection
+    @CollectionTable(name = "base_character_stats", joinColumns = {
+            @JoinColumn(name = "base_character_id")
+    })
+    public List<StatEmbeddable> stats;
+
+    public BaseCharacterEntity(
+            Long characterId, String name, int level, int exp, int remainingHealth,
+            @Nullable Integer currentEquipedItemId, CharacterClass characterClass,
+            CharacterState characterState, boolean moved, List<StatEmbeddable> stats
+    ) {
+        this.characterId = characterId;
+        this.name = name;
+        this.level = level;
+        this.exp = exp;
+        this.remainingHealth = remainingHealth;
+        this.currentEquipedItemId = currentEquipedItemId;
+        this.characterClass = characterClass;
+        this.characterState = characterState;
+        this.moved = moved;
+        this.stats = stats;
+    }
+
+    private BaseCharacterEntity() {
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        BaseCharacterEntity that = (BaseCharacterEntity) o;
+        return Objects.equals(name, that.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name);
+    }
+}
