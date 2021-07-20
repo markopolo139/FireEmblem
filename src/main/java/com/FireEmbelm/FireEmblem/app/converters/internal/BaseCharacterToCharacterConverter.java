a53384b5e4ecba942855ddb10ef7711b8dd87c70
markopolo139 <marekseget@onet.pl>
2021-07-20 17:13:45 +0200

    refractor(converters): delete component in baseCharacter converter

--- src/main/java/com/FireEmbelm/FireEmblem/app/converters/internal/BaseCharacterToCharacterConverter.java
+++ src/main/java/com/FireEmbelm/FireEmblem/app/converters/internal/BaseCharacterToCharacterConverter.java
@@ -6,7 +6,6 @@ import org.springframework.stereotype.Component;
 
 import java.util.List;
 
-@Component
 public interface BaseCharacterToCharacterConverter {
 
     CharacterEntity convertToCharacterEntity(BaseCharacterEntity baseCharacterEntity);
