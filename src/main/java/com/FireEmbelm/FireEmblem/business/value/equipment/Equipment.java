58fa933de3303c67776a5928c5ac2a4322f28967
markopolo139 <marekseget@onet.pl>
2021-07-21 16:56:28 +0200

    feat(equipment): add json type infor

--- src/main/java/com/FireEmbelm/FireEmblem/business/value/equipment/Equipment.java
+++ src/main/java/com/FireEmbelm/FireEmblem/business/value/equipment/Equipment.java
@@ -1,7 +1,23 @@
 package com.FireEmbelm.FireEmblem.business.value.equipment;
 
 import com.FireEmbelm.FireEmblem.business.value.categories.ItemCategory;
+import com.FireEmbelm.FireEmblem.web.models.request.HealingItemModel;
+import com.FireEmbelm.FireEmblem.web.models.request.WeaponModel;
+import com.fasterxml.jackson.annotation.JsonSubTypes;
+import com.fasterxml.jackson.annotation.JsonSubTypes.Type;
+import com.fasterxml.jackson.annotation.JsonTypeInfo;
 
+@JsonTypeInfo(
+        use = JsonTypeInfo.Id.NAME,
+        property = "equipmentType")
+@JsonSubTypes({
+        @Type(value = Weapon.class, name = "weapon"),
+        @Type(value = WeaponModel.class, name = "weaponModel"),
+        @Type(value = HealingItemWithUses.class, name = "healingItem"),
+        @Type(value = HealingItemModel.class, name = "healingItemModel"),
+        @Type(value = Seals.class, name = "seals"),
+        @Type(value = StatsUpItems.class, name = "statsUpItems")
+})
 public interface Equipment {
 
     String getName();
